31c5f39552b8619aaa764fb0315fe56e9a008eea
LeandroAmaral1884 <101856199+LeandroAmaral1884@users.noreply.github.com>
2023-11-29 20:48:56 -0300

    ArrayRemove

---
+++ ArrayListRemoveElementos/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+var lista = new ArrayList() { "Paulo", 5, true, "", null, 1.1, "Zé", 3, 9, null };
+
+
+lista.Remove(null);
+lista.RemoveAt(4);
+lista.RemoveRange(0, 2);
+
+Console.ReadLine();
